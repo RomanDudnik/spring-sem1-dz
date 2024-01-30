99f75c5eee01f279324ddcae84bcaa6eebedc5cd
«Roman <research777@mail.ru>
2024-01-30 20:19:47 +0300

    gs

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -22,8 +22,16 @@ public class Person {
         return firstName;
     }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
     public String getLastName() {
         return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public int getAge() {
